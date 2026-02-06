e3920de7d86e798436424181cf43d019840a57d3
Lohan Hébert <lohanhebert7@gmail.com>
2026-02-06 16:11:56 +0100

    init: create spring

---
+++ src/main/java/com/example/parkingmanagerapi/ParkingManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parkingmanagerapi/ParkingManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
