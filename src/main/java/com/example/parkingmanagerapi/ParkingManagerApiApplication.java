7ac62add1856178e28bfe1129d8d1267f66e678e
Lohan Hébert <lohanhebert7@gmail.com>
2026-02-10 14:30:41 +0100

    refactor: key secure

--- src/main/java/com/example/parkingmanagerapi/ParkingManagerApiApplication.java
+++ src/main/java/com/example/parkingmanagerapi/ParkingManagerApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.parkingmanagerapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParkingManagerApiApplication {
 
     public static void main(String[] args) {
+
+        //on récupérer les valeurs du .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
         SpringApplication.run(ParkingManagerApiApplication.class, args);
     }
 
