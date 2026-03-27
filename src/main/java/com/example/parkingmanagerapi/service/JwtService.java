52e10434b83abacdfe46e14914994fc9be5f3b0a
BillyBoy16 <lennyloy07@gmail.com>
2026-03-27 10:06:31 +0100

    fix: jwt

--- src/main/java/com/example/parkingmanagerapi/service/JwtService.java
+++ src/main/java/com/example/parkingmanagerapi/service/JwtService.java
@@ -47,7 +47,7 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
-    // Pour extraire une info précise
+    // Pour extraire une info précise voulu
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
